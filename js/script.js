435d0fa04a43f3b52e00b205fe8607cceeb38842
sanchez matias <matias-leandro@outlook.com>
2020-01-19 11:49:51 -0300

    se cambio el codigo jquery a js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-jQuery('document').ready(function($)
+/*jQuery('document').ready(function($)
 {
     var menuBtn= $('.hamburguer-icon');
     var menu = $('.navigation ul');
@@ -15,4 +15,23 @@ jQuery('document').ready(function($)
         }
     });
 
+});*/
+
+window.addEventListener('load', () =>
+{
+    let menuBtn = document.querySelector('.hamburguer-icon');
+    let menu = document.querySelector('.navigation ul');
+
+    menuBtn.addEventListener('click', () =>
+    {
+        if(menu.classList.contains('show'))
+        {
+            menu.classList.remove('show');
+        }
+        else
+        {
+            menu.classList.add('show');
+        }
+    });
 });
+
